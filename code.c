246a8e9481b78a9db523d5cea6f0939d719bd4ba
20231021 <2124781855@qq.com>
2024-07-21 21:41:56 +0800

    第三次修改

--- code.c
+++ code.c
@@ -3,7 +3,12 @@ int main(void)
 {
 
 	printf("testing code1../n");
+	printf("testing code2../n");
+	printf("testing code3../n");
+	printf("testing code4../n");
+	printf("testing code5../n");
+	printf("testing code7../n");
+	printf("testing code8../n");
+	printf("testing code9../n");
 	return 0;
-
-
-}
+}	
