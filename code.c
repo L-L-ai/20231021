955db207437cb5c91ebfd3f498f265c5f5dd114d
L-L-ai <2124781855@qq.com>
2024-07-22 12:50:04 +0800

    Update code.c

--- code.c
+++ code.c
@@ -2,13 +2,7 @@
 int main(void)
 {
 
-	printf("testing code0../n");
-	printf("testing code2../n");
-	printf("testing code3../n");
-	printf("testing code4../n");
-	printf("testing code5../n");
-	printf("testing code7../n");
-	printf("testing code8../n");
-	printf("testing code9../n");
+	printf("testing code1../n");
+	
 	return 0;
 }	
